1333486a85bf141c82e4984a7cc295c8024c39e6
arafat <arafat@alphatech.id>
2019-04-03 21:09:56 +0700

    chtaroom 0.1

--- src/Data/Components/ListPlayerChattingRoom.js
+++ src/Data/Components/ListPlayerChattingRoom.js
@@ -8,18 +8,24 @@ class MyBookingComp extends Component {
 
   render() {
     return (
-            <tr>
-              <th scope="row">1</th>
-              <td>{this.props.name}</td>
-              <td>
-                <img
-                  src={this.props.image}
-                  alt="barbarian"
-                  width= "30px"
-                  height= "30px"
-                />
-                </td>
-            </tr>
+            // <tr>
+            //   <th scope="row">1</th>
+            //   <td>{this.props.name}</td>
+            //   <td>
+    
+                <div className="col-lg-3 col-md-3 col-sm-6" style={{textAlign:"center"}}>
+                  <img
+                    src={this.props.image}
+                    alt="barbarian"
+                    width= "130px"
+                    height= "130px"
+                    style={{borderRadius:"50%"}}  
+                  /><br />
+                  <span >{this.props.name}</span>
+            </div>
+
+            // </td>
+            // </tr>
     );
   }
 }
